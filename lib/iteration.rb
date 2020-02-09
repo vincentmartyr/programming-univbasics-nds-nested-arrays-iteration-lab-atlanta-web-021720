2848ecc8967ed1ca5b5025b237e72f3753c45580
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-09 01:02:09 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,18 @@
+#require 'pry'
 def join_ingredients(src)
+inner_results = []
+row_index = 0
+  while row_index < src.count do
+    #binding.pry
+    #while element_index < src[row_index].count do
+      inner_results << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+      row_index += 1
+end
+
+inner_results
+
+end
+
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
@@ -6,13 +20,30 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-end
+
 
 def find_greater_pair(src)
+
+inner_results = []
+row_index = 0
+
+while row_index < src.count do
+    # How to read the following line of code:
+    #   Array at row_index
+    #   Element of the inner array at element_index
+    #   The first character of that element...
+    #if src[row_index][element_index]
+      inner_results << src[row_index].max
+      row_index += 1
+end
+
+inner_results
+
+end
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-end
+
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
@@ -22,4 +53,16 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+total = 0
+row_index = 0
+  while row_index < src.count do
+
+    if src[row_index][0].even? == true && src[row_index][1].even? == true
+       total += src[row_index][0] + src[row_index][1]
+    end
+
+   
+    row_index += 1
+end
+  total
 end
